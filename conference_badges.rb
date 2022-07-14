90f667ac1ac62dfa846180642ad3d729b6a0bb84
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-07-13 17:06:37 -0700

    Completed lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{ name }."
+end
+
+def batch_badge_creator(names)
+  names.map { |name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+  speakers.map.with_index { |speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer(names)
+  batch_badge_creator(names).each {|name| puts name} 
+  assign_rooms(names).each { |name| puts name}
+end
